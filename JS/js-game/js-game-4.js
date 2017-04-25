b50b0c8ff9291f7b4176410e1a04a905a7f9e045
ISWangYe <260777532@qq.com>
2017-04-25 16:45:58 +0800

    js任务2 和其他的文件

---
+++ JS/js-game/js-game-4.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Administrator on 2017/4/23.
+ */
+var num1=document.getElementById("num1");
+var num2=document.getElementById("num2");
+function getNum1() {
+    if (num1.value<4||num1.value>18){
+        alert("请输入正确的人数范围:4~18");
+        num1.value=null;
+    }
+    else {
+        num2.value=num1.value;
+    }
+}
+function getNum2() {
+    num1.value=num2.value;
+}
+function addNum() {
+    num2.value++;
+    num1.value=num2.value;
+}
+function lesNum() {
+    num2.value--;
+    num1.value=num2.value;
+}
+var all=[];
+var killer=[];
+var allid=document.getElementById("allid");
+function peop() {
+    for (var a=0;a<num1.value; a++){
+        all[a]="平民";
+    }
+    all.length=num1.value;
+    killer=Math.floor(all.length/4);
+    for (var random,i=0; i<killer; i++){
+        do {
+            random=Math.floor(Math.random()*all.length)
+        }
+        while (all[random]==="杀手"){
+            all[random]="杀手";
+        }
+    }
+    // Group=all;
+}
+function allot() {
+    peop();
+    var toscreen="";
+    for (var i=0;i<all.length;i++){
+        toscreen=toscreen+"<span class='main-fenpei-right-son'>"+(i+1)+"号身份："+all[i]+"</span>";
+    }
+    console.log(all);
+
+    allid.innerHTML=toscreen;
+}
+function next() {
+    if (all.length!=num1.value){
+        alert("请先设置人数");
+    }
+    else {
+        window.location.href="www.baidu.com";
+    }
+}
